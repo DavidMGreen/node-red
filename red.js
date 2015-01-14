0f5976ce533f312157b59766831046e0d39a8ebe
skhwang <skhwang@users.noreply.github.com>
2015-01-14 16:03:42 -0500

    Test commit

--- red.js
+++ red.js
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+
+// Forked copy for logging and monitoring testing
+
 var http = require('http');
 var https = require('https');
 var util = require("util");
